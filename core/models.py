e9a6847485a48f462797c4025129ad03c764ea15
kimmk4369 <killer5634@naver.com>
2021-03-21 21:07:27 +0900

    Room Model

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stapmed Model """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)  # model 저장시 자동으로 날짜 기록
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True  # 데이터베이스에 등록 X
