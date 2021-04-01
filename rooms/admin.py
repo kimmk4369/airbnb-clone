c6bf4808a9527a07b3d1fc475db0c3a7b9b1d5a7
kimmk4369 <killer5634@naver.com>
2021-04-01 23:00:26 +0900

    Update admin.py

--- rooms/admin.py
+++ rooms/admin.py
@@ -44,7 +44,10 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
+
+    # ordering = ("name", "price", "bedrooms")
 
     list_filter = (
         "instant_book",
@@ -61,6 +64,9 @@ class RoomAdmin(admin.ModelAdmin):
     search_fields = ("^city", "^host__username")  # ^ start with 옵션
 
     filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):  # self:RoomAdmin, obj:List의 현재 Row
+        return obj.amenities.count()
 
 
 @admin.register(models.Photo)
