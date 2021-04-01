9099b218bdf3b10cb3a4de3a42d013b4704b227d
kimmk4369 <killer5634@naver.com>
2021-04-01 22:38:01 +0900

    Room Admin Panel part Two

--- rooms/models.py
+++ rooms/models.py
@@ -86,10 +86,10 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE)  # user 연결
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
-    amenity = models.ManyToManyField("Amenity", blank=True)
-    facility = models.ManyToManyField("Facility", blank=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
     house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
