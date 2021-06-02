3c84acdff6595c4a4ac81d5c30583554c26ddd58
kimmk4369 <killer5634@naver.com>
2021-06-02 15:46:48 +0900

    change model/admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,6 +7,11 @@ class ItemAdmin(admin.ModelAdmin):
 
     """  Item Admin Definition """
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -45,6 +50,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     # ordering = ("name", "price", "bedrooms")
@@ -67,6 +73,9 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_amenities(self, obj):  # self:RoomAdmin, obj:List의 현재 Row
         return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
