d92328bfcd78c2850a1a32c9ee40de4b75cd3fcd
kimmk4369 <killer5634@naver.com>
2021-06-19 10:55:00 +0900

    github login

--- users/admin.py
+++ users/admin.py
@@ -20,6 +20,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -39,4 +40,5 @@ class CustomUserAdmin(UserAdmin):
         "is_staff",
         "is_superuser",
         "email_verified",
+        "login_method",
     )
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.conf import settings
+from django.contrib.auth import login
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.mail import send_mail
@@ -24,6 +25,17 @@ class User(AbstractUser):
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGIN_KAKAO, "Kakao"),
+    )
+
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
@@ -37,6 +49,9 @@ class User(AbstractUser):
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=20, default="", blank=True)
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
 
     def verify_email(self):
         if self.email_verified is False:
