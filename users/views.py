5720ff695c977cc8d321c044474529ed276734b9
kimmk4369 <killer5634@naver.com>
2021-06-29 12:30:00 +0900

    use messageFramework

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import redirect, render, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.base import ContentFile
+from django.contrib import messages
 from . import forms, models
 
 
@@ -84,7 +85,7 @@ def github_callback(request):
             token_json = token_request.json()
             error = token_json.get("error", None)
             if error is not None:
-                raise GithubException()
+                raise GithubException("Can't get access token.")
             else:
                 access_token = token_json.get("access_token")
                 profile_request = requests.get(
@@ -105,7 +106,9 @@ def github_callback(request):
                         # user가 존재하는 경우
                         user = models.User.objects.get(email=email)
                         if user.login_method != models.User.LOGIN_GITHUB:
-                            raise GithubException()
+                            raise GithubException(
+                                f"please log in with: {user.login_method}"
+                            )
                     except models.User.DoesNotExist:
                         user = models.User.objects.create(
                             email=email,
@@ -119,13 +122,15 @@ def github_callback(request):
                         user.save()
 
                     login(request, user)
+                    messages.success(request, f"Welcome back {user.first_name}!")
                     return redirect(reverse("core:home"))
                 else:
-                    raise GithubException()
+                    raise GithubException("Can't get your profile.")
         else:
-            raise GithubException()
-    except GithubException:
+            raise GithubException("Can't get your code.")
+    except GithubException as e:
         # send error message
+        messages.error(request, e)
         return redirect(reverse("users:login"))
 
 
@@ -152,7 +157,7 @@ def kakao_callback(request):
         token_json = token_request.json()
         error = token_json.get("error", None)
         if error is not None:
-            raise KakaoException()
+            raise KakaoException("Can't get authorization code.")
         access_token = token_json.get("access_token")
         profile_request = requests.get(
             "https://kapi.kakao.com/v2/user/me",
@@ -162,14 +167,14 @@ def kakao_callback(request):
         print(profile_json)
         email = profile_json.get("kakao_account").get("email")
         if email is None:
-            raise KakaoException()
+            raise KakaoException("Email error")
         properties = profile_json.get("properties")
         nickname = properties.get("nickname")
         profile_image = properties.get("profile_image")
         try:
             user = models.User.objects.get(email=email)
             if user.login_method != models.User.LOGIN_KAKAO:
-                raise KakaoException()
+                raise KakaoException(f"please log in with: {user.login_method}")
         except models.User.DoesNotExist:
             user = models.User.objects.create(
                 email=email,
@@ -186,6 +191,8 @@ def kakao_callback(request):
                     f"{nickname}-avatar", ContentFile(photo_request.content)
                 )
         login(request, user)
+        messages.success(request, f"Welcome back {user.first_name}!")
         return redirect(reverse("core:home"))
-    except KakaoException:
+    except KakaoException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
