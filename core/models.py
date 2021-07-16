9e7775ea6b1865f24ed8b6fd7f02d53eb6b2049e
kimmk4369 <killer5634@naver.com>
2021-07-16 13:58:48 +0900

    review

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from . import managers
 
 # Create your models here.
 class TimeStampedModel(models.Model):
@@ -7,6 +8,7 @@ class TimeStampedModel(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)  # model 저장시 자동으로 날짜 기록
     updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomModelManager()
 
     class Meta:
         abstract = True  # 데이터베이스에 등록 X
